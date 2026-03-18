f6c74af19a9dfd683557217672b866f6364d402d
Mike <mikerichfaulkner@gmail.com>
2026-03-18 20:38:20 +0000

    update verification token

--- app/api/ebay/account-deletion/route.js
+++ app/api/ebay/account-deletion/route.js
@@ -1,7 +1,7 @@
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const challenge_code = searchParams.get('challenge_code');
-  const verification_token = 'boxxond021020';
+  const verification_token = 'boxxond021020PackerfulCharlotte87';
   const endpoint = 'https://boxxond.com/api/ebay/account-deletion';
 
   const crypto = await import('crypto');
