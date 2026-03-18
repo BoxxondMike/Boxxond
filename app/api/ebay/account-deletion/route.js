9535d14f3b28d07d7193e64d166129ec32c37c17
Mike <mikerichfaulkner@gmail.com>
2026-03-18 20:34:47 +0000

    add ebay account deletion endpoint

---
+++ app/api/ebay/account-deletion/route.js
@@ -0,0 +1,18 @@
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const challenge_code = searchParams.get('challenge_code');
+  const verification_token = 'boxxond021020';
+  const endpoint = 'https://boxxond.com/api/ebay/account-deletion';
+
+  const crypto = await import('crypto');
+  const hash = crypto.default
+    .createHash('sha256')
+    .update(challenge_code + verification_token + endpoint)
+    .digest('hex');
+
+  return Response.json({ challengeResponse: hash });
+}
+
+export async function POST(request) {
+  return new Response('OK', { status: 200 });
+}
