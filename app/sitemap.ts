c120c906408204a2b8dca4bcf61652f70d3c2363
Mike <mikerichfaulkner@gmail.com>
2026-03-20 16:45:28 +0000

    update sitemap with all set pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,6 +14,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.8,
     },
+    // Soccer
     {
       url: 'https://boxxond.com/sets/topps-chrome',
       lastModified: new Date(),
@@ -46,6 +47,63 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
     {
       url: 'https://boxxond.com/sets/panini-select',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    // Basketball
+    {
+      url: 'https://boxxond.com/sets/panini-prizm-nba',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://boxxond.com/sets/topps-chrome-nba',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://boxxond.com/sets/panini-select-nba',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    // Baseball
+    {
+      url: 'https://boxxond.com/sets/topps-chrome-baseball',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://boxxond.com/sets/topps-baseball',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://boxxond.com/sets/bowman-baseball',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    // NFL
+    {
+      url: 'https://boxxond.com/sets/panini-prizm-nfl',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://boxxond.com/sets/topps-chrome-nfl',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://boxxond.com/sets/panini-select-nfl',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.7,
